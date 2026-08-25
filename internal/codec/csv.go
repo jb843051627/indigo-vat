986081a4e7f5ed05f3a0df825388bb1f3d96bd51
jiabo <843051627@qq.com>
2026-08-26 01:24:30 +0800

    chore: isolate export snapshots

--- internal/codec/csv.go
+++ internal/codec/csv.go
@@ -13,7 +13,8 @@ func WriteSamples(w io.Writer, samples []model.Sample, locationName string) erro
 		return err
 	}
 	location := Location(locationName)
-	for _, sample := range samples {
+	snapshot := append([]model.Sample(nil), samples...)
+	for _, sample := range snapshot {
 		if err := writer.Write([]string{sample.ID, Format(sample.TakenAt, location), strconv.FormatFloat(sample.Hue, 'f', 2, 64), strconv.FormatFloat(sample.PH, 'f', 2, 64), strconv.FormatFloat(sample.Temperature, 'f', 2, 64), sample.Status, sample.Observer}); err != nil {
 			return err
 		}
